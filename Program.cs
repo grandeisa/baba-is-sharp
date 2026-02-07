ed3aaab6caee56c51588dc097dec543d58349b5c
IsaelGabriel <isaelgsfaria@gmail.com>
2026-02-07 00:14:38 -0300

    Add window

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+public static class Game
+{
+
+#region window-variables
+    const int WIDTH = 400, HEIGHT = 400;
+    const int TARGET_FPS = 60;
+
+    static readonly string Title = "Baba is #";
+#endregion
+
+
+    static void Main()
+    {
+        Raylib.InitWindow(WIDTH, HEIGHT, Title);
+
+        Raylib.SetTargetFPS(TARGET_FPS);
+
+        while(!Raylib.WindowShouldClose())
+        {
+            Render();
+        }
+
+        Raylib.CloseWindow();
+    }
+
+    static void Render()
+    {
+        Raylib.ClearBackground(Color.Black);
+
+        Raylib.BeginDrawing();
+            Raylib.DrawFPS(0, 0);
+
+
+        Raylib.EndDrawing();
+    }
+}
